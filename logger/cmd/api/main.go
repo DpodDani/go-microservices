bc9983a0100be6c685e40fd843a9ccecc45567c9
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2024-01-05 20:00:53 +0000

    feat: set up skeleton code for logger service

---
+++ logger/cmd/api/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoUrl = "mongodb://mongo:27017"
+	gRpcport = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct{}
+
+func main() {
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI(mongoUrl)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		fmt.Printf("🛑 - Couldn't connect to MongoDB using: %s\n", mongoUrl)
+		return nil, err
+	}
+
+	return client, err
+}
