c80b0fdaba65975446ef3d9ec2b200e91e4b442e
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2024-02-11 00:00:15 +0000

    ref: establish RMQ connection

--- listener/main.go
+++ listener/main.go
@@ -1,8 +1,54 @@
 package main
 
+import (
+	"log"
+	"math"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
 func main() {
 	// try to connect to RabbitMQ
+	conn, err := connect()
+	if err != nil {
+		log.Panic("Failed to connect to RabbitMQ ❌")
+	}
+	defer conn.Close()
 	// start listening to messages
 	// create consumer
 	// watch queue and consume events (from a topic)
+}
+
+func connect() (*amqp.Connection, error) {
+	var connection *amqp.Connection
+	var err error
+
+	baseDelay := 1 * time.Second
+	retries := 0
+	maxRetries := 5
+	rmqDsn := "amqp://guest:guest@localhost"
+
+	for {
+		conn, err := amqp.Dial(rmqDsn)
+		if err != nil {
+			log.Printf("Failed to connecto RabbitMQ server at: %s\n", rmqDsn)
+			retries++
+		} else {
+			log.Println("Connected to RMQ ✅")
+			connection = conn
+			break
+		}
+
+		if retries > maxRetries {
+			log.Println("Reached max retry limit. Exiting...")
+			break
+		}
+
+		backOffTime := time.Duration(math.Pow(2, float64(retries))) * baseDelay
+		log.Printf("Backing off for %v...\n", backOffTime)
+		time.Sleep(backOffTime)
+	}
+
+	return connection, err
 }
